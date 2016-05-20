e69bb18466d2638a37b7fe248eb6894c865a50bb
Silvan Mosberger <infinisil@icloud.com>
2016-05-20 12:40:02 +0200

    Initial Commit

---
+++ MetalMemory/MetalMemory.h
@@ -0,0 +1,19 @@
+//
+//  MetalMemoryOSX.h
+//  MetalMemoryOSX
+//
+//  Created by Silvan Mosberger on 20/05/16.
+//
+//
+
+@import Foundation;
+
+//! Project version number for MetalMemoryOSX.
+FOUNDATION_EXPORT double MetalMemoryOSXVersionNumber;
+
+//! Project version string for MetalMemoryOSX.
+FOUNDATION_EXPORT const unsigned char MetalMemoryOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalMemoryOSX/PublicHeader.h>
+
+
---
+++ MetalMemory/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  MetalMemory
+//
+//  Created by Silvan Mosberger on 20/05/16.
+//
+//
+
+import Metal
+
+func test() -> String {
+	return "Test"
+}
