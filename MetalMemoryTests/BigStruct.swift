55b644f2c0128a21bbbb2992bc1c801c449e05d3
Silvan Mosberger <infinisil@icloud.com>
2016-07-27 01:52:38 +0200

    Updated PerfectSize to 1.1

--- MetalMemoryTests/BigStruct.swift
+++ MetalMemoryTests/BigStruct.swift
@@ -9,12 +9,10 @@
 
 import PerfectSize
 
-typealias HalfPage = B.I.O.O.O.O.O.O.O.O.O.O.O
-
-typealias Page = HalfPage.O
-typealias PagePlus1 = HalfPage.I
+typealias Page = B.I.O.O.O.O.O.O.O.O.O.O.O.O
+typealias PagePlus1 = Page.Add1
 
 typealias PageTimes8 = Page.O.O.O
-typealias PageTimes8Plus1 = Page.O.O.I
+typealias PageTimes8Plus1 = PageTimes8.Add1
 
 typealias TenThousand = D.n1.n0.n0.n0.n0
