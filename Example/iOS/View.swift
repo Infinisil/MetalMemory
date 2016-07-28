8bd3ae6dc56a5ebc1dafc2bdae88ade4fd7107af
Silvan Mosberger <infinisil@icloud.com>
2016-07-28 22:49:49 +0200

    iOS Example now works

--- Example/iOS/View.swift
+++ Example/iOS/View.swift
@@ -10,31 +10,36 @@ import UIKit
 import MetalKit
 
 class ViewController: UIViewController {
-
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+		
+		let drawingView = view as! DrawingView
+		drawingView.renderer = Renderer(view: drawingView)
 	}
 }
 
 class DrawingView : MTKView {
 	var renderer : Renderer?
 	
-//	func normalizedPoint(forEvent event: NSEvent) -> float2 {
-//		let point = convertPoint(event.locationInWindow, fromView: window?.contentView)
-//		return float2(Float(point.x / frame.width), Float(point.y / frame.height)) * 2 - float2(1)
-//	}
-//	
-//	override func mouseDown(theEvent: NSEvent) {
-//		renderer?.startLine(atPoint: normalizedPoint(forEvent: theEvent))
-//	}
-//	
-//	override func mouseDragged(theEvent: NSEvent) {
-//		renderer?.appendToLastLine(point: normalizedPoint(forEvent: theEvent))
-//	}
+	var currentTouch : UITouch?
+
+	func touchPoint(forTouch touch: UITouch) -> float2 {
+		let point = touch.locationInView(self)
+		return float2(Float(point.x / frame.width), Float(1 - point.y / frame.height)) * 2 - float2(1)
+	}
+	
+	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		guard let touch = touches.first where currentTouch == nil else { return }
+		currentTouch = touch
+		renderer?.startLine(atPoint: touchPoint(forTouch: touch))
+	}
+
+	override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		guard let touch = currentTouch where touches.contains(touch) else { return }
+		renderer?.appendToLastLine(point: touchPoint(forTouch: touch))
+	}
+	
+	override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		currentTouch = nil
+	}
 }
