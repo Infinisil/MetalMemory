ffcee7a4e92f5b287ace3c87c272fea7ae2ad9e4
Silvan Mosberger <infinisil@icloud.com>
2016-10-13 12:14:08 +0200

    Added test nextPowerOf2

--- Tests/UtilsTests.swift
+++ Tests/UtilsTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class UtilsTests: XCTestCase {
 
-	func testIsPowerOfTwo() {
+	func testIsPowerOf2() {
 		let exp = (0..<10)
 		
 		let powers : Set<Int> = Set(exp.map{ 1 << $0 })
@@ -24,6 +24,21 @@ class UtilsTests: XCTestCase {
 			XCTAssertFalse(i.isPowerOfTwo)
 		}
 		
+	}
+	
+	func testNextPowerOf2() {
+		let exp = (0..<10)
+		
+		let powers = exp.map{ 1 << $0 }
+		
+		XCTAssertEqual(0.nextPowerOf2(), 0)
+		XCTAssertEqual(1.nextPowerOf2(), 1)
+		
+		for (a, b) in zip(powers, powers.dropFirst()) {
+			for i in a+1 ... b {
+				XCTAssertEqual(i.nextPowerOf2(), b)
+			}
+		}
 	}
 	
 }
